39ab60274e147b34e4fb58c6c9a9d891a464d971
Jeff Yeh <jeffyeh770@gmail.com>
2020-01-23 13:20:12 -0600

    added apple tree

--- src/main/java/com/jyeh/appletree/AppleTreeApplication.java
+++ src/main/java/com/jyeh/appletree/AppleTreeApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.DoubleAdder;
 
@@ -16,12 +18,12 @@ public class AppleTreeApplication implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
-        integerTree();
+        //integerTree();
         appleTree();
     }
 
     private void integerTree() {
-        int[] intArray = new int[] {7, 10, 5, 4, 20, 12, 8};
+        int[] intArray = new int[] {7, 10, 10,5, 4, 20, 12, 8};
         BinarySearchTree<Integer> tree = new BinarySearchTree<>();
         tree.setRoot(new TreeNode<>(intArray[0]));
         for (int i = 1; i < intArray.length; i++) {
@@ -41,5 +43,33 @@ public class AppleTreeApplication implements CommandLineRunner {
     }
 
     private void appleTree() {
+        AppleComparator comparator = (t, o) -> (int) Math.floor(t.getPrice() - o.getPrice());
+        Apple[] apples = new Apple[] {
+                Apple.builder().type("Fuji").price(4.99).weight(1.).comparator(comparator).build(),
+                Apple.builder().type("PinkLady").price(5.99).weight(1.).comparator(comparator).build(),
+                Apple.builder().type("GoldenCrispy").price(3.99).weight(1.).comparator(comparator).build(),
+                Apple.builder().type("RedDelicious").price(6.99).weight(1.).comparator(comparator).build()
+        };
+
+        BinarySearchTree<Apple> tree = new BinarySearchTree<>();
+        tree.setRoot(new TreeNode<>(apples[0]));
+        for (int i = 1; i < apples.length; i++) {
+            tree.addNode(new TreeNode<>(apples[i]));
+        }
+
+        tree.traverse(tree.getRoot(), System.out::println);
+
+        DoubleAdder sum = new DoubleAdder();
+        tree.traverse(tree.getRoot(), node -> sum.add(node.getValue().getPrice()));
+        System.out.println("Total Price: " + sum.doubleValue());
+
+        Max max = new Max();
+        tree.traverse(tree.getRoot(), node -> {
+            if (node.getValue().getPrice() > max.max) max.max = node.getValue().getPrice();  });
+        System.out.println(max.max);
+    }
+
+    private class Max {
+        public double max = 0;
     }
 }
